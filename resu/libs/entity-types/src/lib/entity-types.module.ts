fa6ba0f946b2f9ad10299357f796dfc580070231
janesk90 <janesk90@gmail.com>
2021-08-20 18:43:55 -0400

    I actually have no idea what I'm going to do with that yet, maybe relegate to ORM?

--- resu/libs/entity-types/src/lib/entity-types.module.ts
+++ resu/libs/entity-types/src/lib/entity-types.module.ts
@@ -4,32 +4,5 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-interface Entity {
-
-}
-export class Accolades implements Entity {
-
-}
-export class AccoladesToPeople implements Entity {
-
-}
-export class Companies implements Entity {
-
-}
-export class Persons implements Entity {
-
-}
-export class Positions implements Entity {
-
-}
-export class PositionsNotes implements Entity {
-
-}
-export class Skills implements Entity {
-
-}
-export class SkillsToPeople implements Entity {
-
-}
 
 export class EntityTypesModule {}
