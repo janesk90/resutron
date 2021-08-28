5a6ed5bdb78c178cc72c79cbbf6db0d0b5102b0e
janesk90 <janesk90@gmail.com>
2021-08-28 17:12:24 -0400

    it was too ugly

--- resu/apps/reserver/src/main.ts
+++ resu/apps/reserver/src/main.ts
@@ -6,38 +6,28 @@
 import * as express from 'express';
 import { AccoladesDAO, AccoladesToPersonsDAO, AccoladeTypesDAO, CompaniesDAO, PersonsDAO, PositionNotesDAO, PositionsDAO, SkillsDAO, SkillsToPersonsDAO } from '../../../libs/resume-entity/src/lib/resume-entity.module';
 
+let endpointMap = {
+  accolade_types: AccoladeTypesDAO,
+  accolades: AccoladesDAO,
+  accolades_to_persons: AccoladesToPersonsDAO,
+  companies: CompaniesDAO,
+  persons: PersonsDAO,
+  positions: PositionsDAO,
+  position_notes: PositionNotesDAO,
+  skills: SkillsDAO,
+  skills_to_persons: SkillsToPersonsDAO
+};
+
 const app = express();
 
 app.get('/api/:endpoint', async (req, res) => {
-  let d = {
-    accolade_types: AccoladeTypesDAO,
-    accolades: AccoladesDAO,
-    accolades_to_persons: AccoladesToPersonsDAO,
-    companies: CompaniesDAO,
-    persons: PersonsDAO,
-    positions: PositionsDAO,
-    position_notes: PositionNotesDAO,
-    skills: SkillsDAO,
-    skills_to_persons: SkillsToPersonsDAO
-  };
-  let mdao = new d[req.params.endpoint]({ host: 'localhost', user: 'root', password: 'root', database: 'kr' });
+  let mdao = new endpointMap[req.params.endpoint]({ host: 'localhost', user: 'root', password: 'root', database: 'kr' });
   let x = await mdao.getAll();
   res.send(x);
 });
 
 app.get('/api/:endpoint/:id', async (req, res) => {
-  let d = {
-    accolade_types: AccoladeTypesDAO,
-    accolades: AccoladesDAO,
-    accolades_to_persons: AccoladesToPersonsDAO,
-    companies: CompaniesDAO,
-    persons: PersonsDAO,
-    positions: PositionsDAO,
-    position_notes: PositionNotesDAO,
-    skills: SkillsDAO,
-    skills_to_persons: SkillsToPersonsDAO
-  };
-  let mdao = new d[req.params.endpoint]({ host: 'localhost', user: 'root', password: 'root', database: 'kr' });
+  let mdao = new endpointMap[req.params.endpoint]({ host: 'localhost', user: 'root', password: 'root', database: 'kr' });
   let x: any;
   if (req.params.id) {
     x = await mdao.getOne(parseInt(req.params.id));
