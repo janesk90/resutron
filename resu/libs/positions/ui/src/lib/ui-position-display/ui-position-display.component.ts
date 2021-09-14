0a4a029be96b236f54529595a5223923ded3882a
janesk90 <janesk90@gmail.com>
2021-09-14 19:12:55 -0400

    all that's left is to add the education section and then I guess this front page will be done-- need to add a getByPersonId to skills as well, I think

--- resu/libs/positions/ui/src/lib/ui-position-display/ui-position-display.component.ts
+++ resu/libs/positions/ui/src/lib/ui-position-display/ui-position-display.component.ts
@@ -8,9 +8,13 @@ import { Positions } from '@resu/positions/domain';
 })
 export class UiPositionDisplayComponent implements OnInit {
   @Input() position!: Positions;
+  start!: any;
+  end!: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.start = new Date(this.position.positions_start).toLocaleDateString("en-US");
+    this.end = new Date(this.position.positions_end).toLocaleDateString("en-US");
   }
 
 }
